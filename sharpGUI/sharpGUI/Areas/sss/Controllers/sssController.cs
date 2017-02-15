ca329fbbfc9c5aaccad1cc94731cd1d3015fb5d6
advence-liz <1012515183@qq.com>
2017-02-15 10:56:08 +0800

    修改目录结构

---
+++ sharpGUI/sharpGUI/Areas/sss/Controllers/sssController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sharpGUI.Areas.sss.Controllers
+{
+    public class sssController : Controller
+    {
+        // GET: sss/sss
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ sharpGUI/sharpGUI/Areas/sss/sssAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace sharpGUI.Areas.sss
+{
+    public class sssAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "sss";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "sss_default",
+                "sss/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
